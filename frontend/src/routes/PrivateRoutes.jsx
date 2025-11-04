cf73c30632f1a48c1835602bdd42422cc4973437
asif ahemmed <asifahemmed0@gmail.com>
2025-11-04 15:30:03 +0800

    code refactor with reducers

--- frontend/src/routes/PrivateRoutes.jsx
+++ frontend/src/routes/PrivateRoutes.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Header from "../components/common/Header";
 import { useAuth } from "../hooks/useAuth";
+import ProfileProvider from "../providers/ProfileProvider";
 
 
 const PrivateRoutes = () => {
@@ -8,15 +9,17 @@ const PrivateRoutes = () => {
     return (
         <>
             {
-                auth.user ?
+                auth.authToken ?
                     (
                         <>
-                            <Header />
-                            <main className="mx-auto max-w-[1020px] py-8">
-                                <div className="container">
-                                    <Outlet />
-                                </div>
-                            </main>
+                            <ProfileProvider>
+                                <Header />
+                                <main className="mx-auto max-w-[1020px] py-8">
+                                    <div className="container">
+                                        <Outlet />
+                                    </div>
+                                </main>
+                            </ProfileProvider>
                         </>
                     ) :
                     (
